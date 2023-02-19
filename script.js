019f5cfce034cefd8a6dbed11981b3a5caae60c5
Viper910 <beheraamitkumar156@gmail.com>
2023-02-19 11:55:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+const typed = new Typed(".learnings", {
+  strings: ["Website", "Mobile Application","Game", "UI/UX"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true,
+});
+
+const skill = new Typed(".skills-section",{
+  strings:[""],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true,
+})
